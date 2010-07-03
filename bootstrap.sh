648832404d207693406ab97260b194570834d690
Ari Lerner <arilerner@mac.com>
2010-07-02 20:23:42 -0700

    Removed rabbit, for the time being

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,26 +10,26 @@ mkdir -p $DEPSDIR
 cd $DEPSDIR
 
 # Make sure rabbitmq-server is available
-(  
-  RABBIT_TAR="rabbitmq-server-$RABBIT_VERSION.tar.gz"
-  if [ ! -d "$DEPSDIR/rabbitmq-server" ]; then
-    curl -O "http://www.rabbitmq.com/releases/rabbitmq-server/v$RABBIT_VERSION/$RABBIT_TAR"
-    tar -zxf $RABBIT_TAR
-    mv "rabbitmq-server-$RABBIT_VERSION" "rabbitmq-server"
-    rm $RABBIT_TAR
-  fi
-  cd "rabbitmq-server"
-  make
-)
+# (  
+#   RABBIT_TAR="rabbitmq-server-$RABBIT_VERSION.tar.gz"
+#   if [ ! -d "$DEPSDIR/rabbitmq-server" ]; then
+#     curl -O "http://www.rabbitmq.com/releases/rabbitmq-server/v$RABBIT_VERSION/$RABBIT_TAR"
+#     tar -zxf $RABBIT_TAR
+#     mv "rabbitmq-server-$RABBIT_VERSION" "rabbitmq-server"
+#     rm $RABBIT_TAR
+#   fi
+#   cd "rabbitmq-server"
+#   make
+# )
 
 # Make sure rabbitmq-erlang-client is available
-(
-  if [ ! -d "$DEPSDIR/rabbitmq-erlang-client" ]; then
-    hg clone http://hg.rabbitmq.com/rabbitmq-erlang-client/
-  fi
-  cd rabbitmq-erlang-client
-  make 
-)
+# (
+#   if [ ! -d "$DEPSDIR/rabbitmq-erlang-client" ]; then
+#     hg clone http://hg.rabbitmq.com/rabbitmq-erlang-client/
+#   fi
+#   cd rabbitmq-erlang-client
+#   make 
+# )
 
 # Make sure local132 is available
 (
